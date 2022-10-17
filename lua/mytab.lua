d46651275fbf233ebdc90dcf303d23c1715c1545
noob <noob@noob.noob>
2022-10-17 09:02:00 +0300

    buffnew buffenter

--- lua/mytab.lua
+++ lua/mytab.lua
@@ -1,7 +1,7 @@
 local autocmd = vim.api.nvim_create_autocmd   -- Create autocommand
 
 vim.api.nvim_create_augroup("myTabGroup", {clear=true})
-autocmd('BufEnter', {
+autocmd({'BufEnter', 'BufNew'}, {
   pattern = { '*.exs', '*.ex' },
   command = 'setlocal tabstop=2 shiftwidth=2',
   group = "myTabGroup",
