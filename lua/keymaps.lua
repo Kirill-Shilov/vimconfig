473ab08c9f7450ea4615744be0dad606474ef7c5
noob <noob@noob.noob>
2022-10-23 03:17:00 +0300

    exit term

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -6,7 +6,10 @@ local tree = require('nvim-tree')
 
 cmd 'let g:leadermap = " "'
 map('n', '<leader>p', '"_dP', {noremap = true})
+-- escape from terminal
+vim.keymap.set('t', '<Esc>', '<C-\\><C-n>', defaults)
 vim.keymap.set('', "<C-n>", ':NvimTreeToggle<CR>', {})
+-- telescope
 vim.keymap.set('n', '<leader>fg', builtin.find_files, {})
 vim.keymap.set('n', '<leader>ff', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
